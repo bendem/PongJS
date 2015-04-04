7d72fd90f71c3419ac9c12f08194a26cae60cc51
bendem <online@bendem.be>
2015-04-04 16:32:43 +0200

    Unindent the code a bit and split var declarations

--- pong.js
+++ pong.js
@@ -41,6 +41,7 @@ var h = $pong.offsetHeight;
 var ctx = $pong.getContext('2d');
 var lifes = 3;
 var game_loop;
+var objects = [];
 
 /**
  * Utility functions
@@ -78,125 +79,123 @@ var emptyFunction = function() {};
 
 // I don't want a single monster file, but this is meant for gh-pages
 // so yeah.
-window.addEventListener('load', function() { requirejs(
-    libs,
-    function() {
-        /**
-         * Actual code
-         */
-        var platform = new Platform(
-                new Point(
-                    w / 2 - platformWidth / 2,
-                    h - platformHeight - 5
-                ),
-                platformWidth,
-                platformHeight
-            )
-            , ball = new Ball(
-                new Point(w / 2, h / 5),
-                new Vector(0.1, 0.3).setRotation(Math.random() * 2 * Math.PI),
-                ballRadius,
-                1.02,
-                600
-            )
-            , timer = new Timer(
-                new Point(0, 0),
-                'rgba(255, 255, 255, 0.8)',
-                '1.1rem sans-serif'
-            )
-            , lifeCounter = new LifeCounter(
-                new Point(w - 15, 15),
-                lifes,
-                22
-            )
-            , objects = [
-                ball,
-                platform,
-                timer,
-                lifeCounter,
-                new Wall(new Point(0, 0), false, true),
-                new Wall(new Point(0, 0), true, false),
-                new Wall(new Point(w, 0), false, true),
-            ]
-            , sizeChangedHandler = function() {
-                if($pong.offsetWidth !== w) {
-                    objects.forEach(function(object) {
-                        object.containerWidthChanged($pong.offsetWidth);
-                    });
-                }
-                if($pong.offsetHeight !== h) {
-                    objects.forEach(function(object) {
-                        object.containerHeightChanged($pong.offsetHeight);
-                    });
-                }
+window.addEventListener('load', function() { requirejs(libs, function() {
+    /**
+     * Actual code
+     */
+    var platform = new Platform(
+        new Point(
+            w / 2 - platformWidth / 2,
+            h - platformHeight - 5
+        ),
+        platformWidth,
+        platformHeight
+    );
+    var ball = new Ball(
+        new Point(w / 2, h / 5),
+        new Vector(0.1, 0.3).setRotation(Math.random() * 2 * Math.PI),
+        ballRadius,
+        1.02,
+        600
+    );
+    var timer = new Timer(
+        new Point(0, 0),
+        'rgba(255, 255, 255, 0.8)',
+        '1.1rem sans-serif'
+    );
+    var lifeCounter = new LifeCounter(
+        new Point(w - 15, 15),
+        lifes,
+        22
+    );
+    var sizeChangedHandler = function() {
+        if($pong.offsetWidth !== w) {
+            objects.forEach(function(object) {
+                object.containerWidthChanged($pong.offsetWidth);
+            });
+        }
+        if($pong.offsetHeight !== h) {
+            objects.forEach(function(object) {
+                object.containerHeightChanged($pong.offsetHeight);
+            });
+        }
 
-                // Update globals
-                w = $pong.offsetWidth;
-                h = $pong.offsetHeight;
-                // Fix canvas size
-                $pong.width = w;
-                $pong.height = h;
-                // Force redrawing to prevent black frames
-                $pong.dispatchEvent(new CustomEvent('draw', { detail: ctx }));
-            }
-            ;
-
-        // Force canvas to take the size it actually takes.
+        // Update globals
+        w = $pong.offsetWidth;
+        h = $pong.offsetHeight;
+        // Fix canvas size
         $pong.width = w;
         $pong.height = h;
+        // Force redrawing to prevent black frames
+        $pong.dispatchEvent(new CustomEvent('draw', { detail: ctx }));
+    };
 
-        window.addEventListener('resize', sizeChangedHandler);
-        window.addEventListener('deviceorientation', sizeChangedHandler);
+    // Register the objects
+    objects = objects.concat([
+        ball,
+        platform,
+        timer,
+        lifeCounter,
+        new Wall(new Point(0, 0), false, true),
+        new Wall(new Point(0, 0), true, false),
+        new Wall(new Point(w, 0), false, true),
+    ]);
 
-        window.addEventListener('mousemove', function(e) {
-            platform.move(e.clientX - $pong.offsetLeft);
+    // Force canvas to take the size it actually takes.
+    $pong.width = w;
+    $pong.height = h;
+
+    window.addEventListener('resize', sizeChangedHandler);
+    window.addEventListener('deviceorientation', sizeChangedHandler);
+
+    window.addEventListener('mousemove', function(e) {
+        platform.move(e.clientX - $pong.offsetLeft);
+    });
+
+    // TODO Keyboard control?
+
+    // Touch handling
+    window.addEventListener('touchmove', function(e) {
+        platform.move(e.changedTouches[0].clientX - $pong.offsetLeft);
+    }, false);
+    window.addEventListener('touchstart', function(e) {
+        platform.move(e.changedTouches[0].clientX - $pong.offsetLeft);
+    }, false);
+
+    $pong.addEventListener('draw', function(e) {
+        var ctx = e.detail;
+        ctx.clearRect(0, 0, w, h);
+        objects.forEach(function(object) {
+            object.draw(ctx);
         });
+    });
 
-        // TODO Keyboard control?
-
-        // Touch handling
-        window.addEventListener('touchmove', function(e) {
-            platform.move(e.changedTouches[0].clientX - $pong.offsetLeft);
-        }, false);
-        window.addEventListener('touchstart', function(e) {
-            platform.move(e.changedTouches[0].clientX - $pong.offsetLeft);
-        }, false);
-
-        $pong.addEventListener('draw', function(e) {
-            var ctx = e.detail;
-            ctx.clearRect(0, 0, w, h);
-            objects.forEach(function(object) {
-                object.draw(ctx);
-            });
+    $pong.addEventListener('update', function(e) {
+        objects.forEach(function(object) {
+            object.update(e.detail, objects);
         });
+    });
 
-        $pong.addEventListener('update', function(e) {
-            objects.forEach(function(object) {
-                object.update(e.detail, objects);
-            });
-        });
+    $pong.addEventListener('life_lost', function() {
+        if(lifeCounter.count-- <= 0) {
+            $pong.dispatchEvent(new Event('game_lost'));
+        }
+    });
 
-        $pong.addEventListener('life_lost', function() {
-            if(lifeCounter.count-- <= 0) {
-                $pong.dispatchEvent(new Event('game_lost'));
-            }
-        });
+    $pong.addEventListener('game_lost', function() {
+        clearInterval(game_loop);
+        console.log('YOU LOST BIATCH!');
+        var text = new LostText(
+            'You lose!',
+            'rgba(255, 150, 150, 0.8)',
+            '2rem sans-serif'
+        );
+        objects.push(text);
+        text.draw(ctx);
+    });
 
-        $pong.addEventListener('game_lost', function() {
-            clearInterval(game_loop);
-            console.log('YOU LOST BIATCH!');
-            var text = new LostText(
-                'You lose!',
-                'rgba(255, 150, 150, 0.8)',
-                '2rem sans-serif'
-            );
-            objects.push(text);
-            text.draw(ctx);
-        });
-
-        game_loop = setInterval(function() {
-            $pong.dispatchEvent(new CustomEvent('draw', { detail: ctx }));
-            $pong.dispatchEvent(new CustomEvent('update', { detail: Date.now() }));
-        }, 1000/fps);
-    }
-); });
+    game_loop = setInterval(function() {
+        $pong.dispatchEvent(new CustomEvent('draw', { detail: ctx }));
+        $pong.dispatchEvent(new CustomEvent('update', { detail: Date.now() }));
+    }, 1000/fps);
+}); });
